9b03f0ffb598acd12262dfd72fd1a3ae5037c9c7
jinseon0328 <wishofsun@daum.net>
2021-04-30 19:28:56 +0900

    2021-04-30

--- app/src/main/java/com/eomcs/web/ex01/Servlet04.java
+++ app/src/main/java/com/eomcs/web/ex01/Servlet04.java
@@ -6,6 +6,7 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 // @WebServlet 애노테이션
 // => web.xml 에 서블릿 정보를 설정하는 대신에 
@@ -19,7 +20,7 @@ import javax.servlet.ServletResponse;
 //@WebServlet(value="/ex01/s04")
 //@WebServlet(urlPatterns="/ex01/s04")
 //@WebServlet(urlPatterns={"/ex01/s04"})
-//@WebServlet(urlPatterns={"/ex01/s04","/ex01/haha","ex01/hoho"})
+@WebServlet(urlPatterns={"/ex01/s04","/ex01/hoho","/hoho2"})
 public class Servlet04 extends GenericServlet {
 
   private static final long serialVersionUID = 1L;
